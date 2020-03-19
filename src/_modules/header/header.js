19ac22c34f781bb5c84599569e7768c5391eeea5
Mike <migueljrojas@gmail.com>
2020-03-19 20:04:51 -0300

    assetes

--- src/_modules/header/header.js
+++ src/_modules/header/header.js
@@ -5,6 +5,12 @@ var Header = function() {
     var header = $('.header');
     var body = $('body');
     var menuOpen = $('.header__hamburguer');
+    var links = $('.header__item a');
+
+    links.on('click', function(){
+      header.removeClass('-open');
+      body.removeClass('-hideOverflow');
+    });
 
     menuOpen.on('click', function(){
         header.toggleClass('-open');
@@ -27,7 +33,37 @@ var Header = function() {
         $(this).addClass('-active');
         contents.filter('[data-content=' + targettedContent + ']').addClass('-active')
 
-    })
+    });
+
+    $('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function(event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+          // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 50
+                }, 1000, function() {
+                // Callback after animation
+                // Must change focus!
+                    var $target = $(target);
+                    $target.focus();
+                });
+            }
+        }
+    });
 };
 
 module.exports = Header;
